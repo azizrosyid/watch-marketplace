a276aa4cb5e4653f2d5b2590b045595612986d5a
Aziz Rosyid <azizrosyid783@gmail.com>
2021-11-19 21:12:11 +0700

    add error prevention

--- database/seeders/StoreSeeder.php
+++ database/seeders/StoreSeeder.php
@@ -61,55 +61,59 @@ class StoreSeeder extends Seeder
         }
 
         foreach ($brandId as $id) {
-            $urlStore = "https://shopee.co.id/api/v2/shop/get?shopid={$id}";
-            $response = file_get_contents($urlStore);
-            $data = json_decode($response, true);
-
-            $user = [
-                'name' => $data['data']['name'],
-                'email' => $data['data']['account']['username'] . "@gmail.com",
-                'username' => $data['data']['account']['username'],
-                'address' => $data['data']['place'],
-                'password' => Hash::make('password'),
-                'remember_token' => Str::random(10),
-            ];
-            $user = User::factory()->create($user);
-
-            $storeLogo = cloudinary()->upload("https://cf.shopee.co.id/file/" . $data['data']['account']['portrait'])->getSecurePath();
-            $store = [
-                'name' => $data['data']['name'],
-                'slug' => Str::slug($data['data']['name']),
-                'description' => $data['data']['description'],
-                'address' => $data['data']['place'],
-                'email' => $data['data']['account']['username'] . "@gmail.com",
-                'logo' => $storeLogo,
-                'user_id' => $user->id,
-            ];
-            $store = Store::factory()->create($store);
-
-            $urlProducts = "https://shopee.co.id/api/v4/search/search_items?by=pop&limit=10&match_id={$id}&newest=0&order=desc&page_type=shop&scenario=PAGE_OTHERS&version=2";
-            $response = file_get_contents($urlProducts);
-            $data = json_decode($response, true);
-
-            foreach ($data["items"] as $item) {
-                $item = $item['item_basic'];
-                $urlDetail = "https://shopee.co.id/api/v4/item/get?itemid={$item['itemid']}&shopid={$id}";
-                $response = file_get_contents($urlDetail);
+            try {
+                $urlStore = "https://shopee.co.id/api/v2/shop/get?shopid={$id}";
+                $response = file_get_contents($urlStore);
                 $data = json_decode($response, true);
-                $detailItem = $data['data'];
 
-                $productImage = cloudinary()->upload("https://cf.shopee.co.id/file/" . $item['image'])->getSecurePath();
-                $product = [
-                    'name' => $detailItem['name'],
-                    'slug' => Str::slug($detailItem['name']),
-                    'description' => $detailItem['description'],
-                    'stock' => $detailItem['stock'],
-                    'price' => $detailItem['price'] / 10000,
-                    'image' => $productImage,
-                    'store_id' => $store->id,
-                    'category_id' => Category::inRandomOrder()->first()->id,
+                $user = [
+                    'name' => $data['data']['name'],
+                    'email' => $data['data']['account']['username'] . "@gmail.com",
+                    'username' => $data['data']['account']['username'],
+                    'address' => $data['data']['place'],
+                    'password' => Hash::make('password'),
+                    'remember_token' => Str::random(10),
                 ];
-                Product::create($product);
+                $user = User::factory()->create($user);
+
+                $storeLogo = cloudinary()->upload("https://cf.shopee.co.id/file/" . $data['data']['account']['portrait'])->getSecurePath();
+                $store = [
+                    'name' => $data['data']['name'],
+                    'slug' => Str::slug($data['data']['name']),
+                    'description' => $data['data']['description'],
+                    'address' => $data['data']['place'],
+                    'email' => $data['data']['account']['username'] . "@gmail.com",
+                    'logo' => $storeLogo,
+                    'user_id' => $user->id,
+                ];
+                $store = Store::factory()->create($store);
+
+                $urlProducts = "https://shopee.co.id/api/v4/search/search_items?by=pop&limit=10&match_id={$id}&newest=0&order=desc&page_type=shop&scenario=PAGE_OTHERS&version=2";
+                $response = file_get_contents($urlProducts);
+                $data = json_decode($response, true);
+
+                foreach ($data["items"] as $item) {
+                    $item = $item['item_basic'];
+                    $urlDetail = "https://shopee.co.id/api/v4/item/get?itemid={$item['itemid']}&shopid={$id}";
+                    $response = file_get_contents($urlDetail);
+                    $data = json_decode($response, true);
+                    $detailItem = $data['data'];
+
+                    $productImage = cloudinary()->upload("https://cf.shopee.co.id/file/" . $item['image'])->getSecurePath();
+                    $product = [
+                        'name' => $detailItem['name'],
+                        'slug' => Str::slug($detailItem['name']),
+                        'description' => $detailItem['description'],
+                        'stock' => $detailItem['stock'],
+                        'price' => $detailItem['price'] / 10000,
+                        'image' => $productImage,
+                        'store_id' => $store->id,
+                        'category_id' => Category::inRandomOrder()->first()->id,
+                    ];
+                    Product::create($product);
+                }
+            } catch (\Exception $e) {
+                continue;
             }
         }
     }
